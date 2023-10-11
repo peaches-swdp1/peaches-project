1d8f7743399b8fa6db5dddaffbc8c48352b0cc41
Kalle Ilves <kalleilv@gmail.com>
2023-10-11 15:34:28 +0300

    use DataJpaTest annoation for tests

--- src/test/java/fi/haagahelia/coolreads/repository/MessageRepositoryTest.java
+++ src/test/java/fi/haagahelia/coolreads/repository/MessageRepositoryTest.java
@@ -2,21 +2,15 @@ package fi.haagahelia.coolreads.repository;
 
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import fi.haagahelia.coolreads.model.Message;
 
-@SpringBootTest
+@DataJpaTest
 class MessageRepositoryTest {
 	@Autowired
 	private MessageRepository messageRepository;
-
-	@BeforeEach
-	void setUp() throws Exception {
-		messageRepository.deleteAll();
-	}
 
 	@Test
 	void findAllReturnsEmptyListWhenNoMessagesExist() {
@@ -24,14 +18,13 @@ class MessageRepositoryTest {
 
 		assertEquals(0, messages.size());
 	}
-	
+
 	@Test
 	void findAllReturnsListOfMessagesWhenMessagesExist() {
 		messageRepository.save(new Message("Hello world!"));
 		List<Message> messages = messageRepository.findAll();
-		
+
 		assertEquals(1, messages.size());
 		assertEquals("Hello world!", messages.get(0).getContent());
 	}
-
 }
