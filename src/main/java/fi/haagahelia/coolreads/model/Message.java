2d9b2664996753de0827d76e94a12efd97c09424
Kalle Ilves <kalleilv@gmail.com>
2023-10-25 12:12:46 +0300

    add format for createdAt attribute

--- src/main/java/fi/haagahelia/coolreads/model/Message.java
+++ src/main/java/fi/haagahelia/coolreads/model/Message.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -16,6 +18,7 @@ public class Message {
 	private Long id;
 	
 	@CreationTimestamp
+	@JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss")
 	private LocalDateTime createdAt;
 
 	@Column(nullable=false)
