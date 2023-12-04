6b8b864da261c53e70ba561da755bfccf499901a
Polina <polina.domracheva@myy.haaga-helia.fi>
2023-12-04 14:44:53 +0200

    Adding CategoryRestController and a getRecommendationsById method

---
+++ src/main/java/fi/haagahelia/coolreads/controller/CategoryRestController.java
@@ -0,0 +1,40 @@
+package fi.haagahelia.coolreads.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import fi.haagahelia.coolreads.model.Category;
+import fi.haagahelia.coolreads.model.ReadingRecommendation;
+import fi.haagahelia.coolreads.repository.CategoryRepository;
+import fi.haagahelia.coolreads.repository.ReadingRecommendationRepository;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryRestController {
+	@Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private ReadingRecommendationRepository readingRepository;
+
+    @GetMapping("/{categoryId}/recommendations")
+    public ResponseEntity<List<ReadingRecommendation>> getRecommendationsByCategoryId(@PathVariable Long categoryId) {
+    	Optional<Category> category = categoryRepository.findById(categoryId);
+    	 
+    	 if (category.isPresent()) {
+             List<ReadingRecommendation> recommendations = readingRepository.findByCategoryOrderByCreatedOnDesc(category.get());
+             return ResponseEntity.ok(recommendations);
+         } else {
+             return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                     .body(null);
+         }
+    }
+}
--- src/main/java/fi/haagahelia/coolreads/repository/ReadingRecommendationRepository.java
+++ src/main/java/fi/haagahelia/coolreads/repository/ReadingRecommendationRepository.java
@@ -5,9 +5,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import fi.haagahelia.coolreads.model.Category;
 import fi.haagahelia.coolreads.model.ReadingRecommendation;
 
 @Repository
 public interface ReadingRecommendationRepository extends JpaRepository<ReadingRecommendation, Long> {
 	List<ReadingRecommendation> findAllByOrderByCreatedOnDesc();
+
+	List<ReadingRecommendation> findByCategoryOrderByCreatedOnDesc(Category category);
 }
