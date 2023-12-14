97a2378adeaee3f589af122555b963e10faa1f82
Prasanna Tamang Bomjan <prasannatmg.400@gmail.com>
2023-12-14 18:04:20 +0200

    Signup implemented

--- src/main/java/fi/haagahelia/coolreads/controller/ReadingRecommendationController.java
+++ src/main/java/fi/haagahelia/coolreads/controller/ReadingRecommendationController.java
@@ -1,8 +1,11 @@
 package fi.haagahelia.coolreads.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -15,17 +18,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import fi.haagahelia.coolreads.model.Category;
 import fi.haagahelia.coolreads.model.ReadingRecommendation;
+import fi.haagahelia.coolreads.repository.AppUserRepository;
 import fi.haagahelia.coolreads.repository.CategoryRepository;
 import fi.haagahelia.coolreads.repository.ReadingRecommendationRepository;
-
 
 @Controller
 public class ReadingRecommendationController {
 	@Autowired
 	private ReadingRecommendationRepository readingRepository;
-	
+
 	@Autowired
 	private CategoryRepository categoryRepository;
+
+	@Autowired
+	private AppUserRepository userRepository;
 
 	@GetMapping("/")
 	public String renderReccomendationlist(Model model) {
@@ -36,65 +42,59 @@ public class ReadingRecommendationController {
 
 	@GetMapping("/recommendations/add")
 	public String renderAddRecommendationForm(Model model) {
-		model.addAttribute("readingRecommendationDto", new AddReadingRecommendationDto());	
+		model.addAttribute("readingRecommendationDto", new AddReadingRecommendationDto());
 		model.addAttribute("categories", categoryRepository.findAll());
 		return "addreadingrecommendation";
 	}
 
 	@PostMapping("/recommendations/add")
-	public String addMessage(@ModelAttribute("readingRecommendationDto") AddReadingRecommendationDto readingRecommendation) {
+	public String addMessage(
+			@ModelAttribute("readingRecommendationDto") AddReadingRecommendationDto readingRecommendation) {
+
 		Category category = categoryRepository.findByName(readingRecommendation.getCategoryName());
 		ReadingRecommendation newReadingRecommendation = new ReadingRecommendation(readingRecommendation.getTitle(),
 				readingRecommendation.getLink(), readingRecommendation.getDescription(), category);
 		readingRepository.save(newReadingRecommendation);
 
-		return "redirect:/"; 
+		return "redirect:/";
 	}
 
 	@GetMapping("/recommendations/edit/{id}")
 	public String renderEditRecommendationForm(@PathVariable Long id, Model model) {
-	    ReadingRecommendation recommendation = readingRepository.findById(id).get();
+		ReadingRecommendation recommendation = readingRepository.findById(id).get();
 
-	    AddReadingRecommendationDto editDto = new AddReadingRecommendationDto(
-	            recommendation.getTitle(),
-	            recommendation.getLink(),
-	            recommendation.getDescription(),
-	            recommendation.getCategory().getName()
-	    );
+		AddReadingRecommendationDto editDto = new AddReadingRecommendationDto(recommendation.getTitle(),
+				recommendation.getLink(), recommendation.getDescription(), recommendation.getCategory().getName());
 
-	    model.addAttribute("readingRecommendationDto", editDto);
-	    model.addAttribute("categories", categoryRepository.findAll());
-	    model.addAttribute("recommendationId", id);
+		model.addAttribute("readingRecommendationDto", editDto);
+		model.addAttribute("categories", categoryRepository.findAll());
+		model.addAttribute("recommendationId", id);
 
-	    return "editrecommendation";
+		return "editrecommendation";
 	}
 
 	@PostMapping("/recommendations/edit/{id}")
-	public String editRecommendation(@PathVariable Long id, @ModelAttribute("readingRecommendationDto") AddReadingRecommendationDto readingRecommendation) {
-	    Category category = categoryRepository.findByName(readingRecommendation.getCategoryName());
-	    ReadingRecommendation updatedRecommendation = new ReadingRecommendation(
-	            readingRecommendation.getTitle(),
-	            readingRecommendation.getLink(),
-	            readingRecommendation.getDescription(),
-	            category
-	    );
-	    updatedRecommendation.setId(id);
-	    readingRepository.save(updatedRecommendation);
+	public String editRecommendation(@PathVariable Long id,
+			@ModelAttribute("readingRecommendationDto") AddReadingRecommendationDto readingRecommendation) {
+		Category category = categoryRepository.findByName(readingRecommendation.getCategoryName());
+		ReadingRecommendation updatedRecommendation = new ReadingRecommendation(readingRecommendation.getTitle(),
+				readingRecommendation.getLink(), readingRecommendation.getDescription(), category);
+		updatedRecommendation.setId(id);
+		readingRepository.save(updatedRecommendation);
 
-	    return "redirect:/";
+		return "redirect:/";
 	}
-	
+
 	@PostMapping("/recommendations/{id}/delete")
 	public String deleteRecommendationBuId(@PathVariable("id") Long readingRecommendationId) {
-	    readingRepository.deleteById(readingRecommendationId);
-	    return "redirect:/";
+		readingRepository.deleteById(readingRecommendationId);
+		return "redirect:/";
 	}
-	
+
 	@PostMapping("/recommendations/delete")
 	public String deleteRecommendation(@RequestParam("id") Long readingRecommendationId) {
-	    readingRepository.deleteById(readingRecommendationId);
-	    return "redirect:/";
+		readingRepository.deleteById(readingRecommendationId);
+		return "redirect:/";
 	}
 
 }
-
